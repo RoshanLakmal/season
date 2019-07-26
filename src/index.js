d470cc5881a3d2fb84e28e2dab6a3b8a75e95390
Roshan <roshanrox911@gmail.com>
2019-07-26 19:06:02 +1000

    added class componet

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    position => console.log(position),
-    err => console.log(err)
-  );
+// const App = () => {
+//   window.navigator.geolocation.getCurrentPosition(
+//     position => console.log(position),
+//     err => console.log(err)
+//   );
 
-  return <div>Hi there!</div>;
-};
+//   return <div>Latitude: </div>;
+// };
 
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => console.log(position),
+      err => console.log(err)
+    );
+
+    return <div>Latitude: </div>;
+  }
+}
 ReactDom.render(<App />, document.querySelector('#root'));
