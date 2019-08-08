6f02f3cdadd7a8e4b786c9458aa8ac2c0c8a9e75
Roshan <roshanrox911@gmail.com>
2019-08-09 00:16:21 +1000

    Redefine the constructor

--- src/index.js
+++ src/index.js
@@ -11,11 +11,7 @@ import ReactDom from 'react-dom';
 // };
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = { lat: null, errorMessage: '' };
-  }
+  state = { lat: null, errorMessage: '' };
 
   componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
